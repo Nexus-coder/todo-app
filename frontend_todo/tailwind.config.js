26a786930b1de23677ee9546c2f53e80eda2ef8e
Andrew Kimani <68616882+Nexus-coder@users.noreply.github.com>
2024-12-27 14:11:53 +0300

    removed require on tailwind animate

--- frontend_todo/tailwind.config.js
+++ frontend_todo/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
 	darkMode: ["class"],
 	content: [
@@ -55,6 +56,6 @@ export default {
 			}
 		}
 	},
-	plugins: [require("tailwindcss-animate")],
+	// plugins: [require("tailwindcss-animate")],
 }
 
