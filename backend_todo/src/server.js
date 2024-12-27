9509bd93cd8fb3d42ce87bd379c1700a76497644
Andrew Kimani <68616882+Nexus-coder@users.noreply.github.com>
2024-12-27 14:48:04 +0300

    added the flow for going to the http server vs http

--- backend_todo/src/server.js
+++ backend_todo/src/server.js
@@ -11,29 +11,34 @@ const fs = require('fs')
 
 
 const app = require("./app");
-const { mongoConnect ,isProduction} = require("./services/mongo");
+const { mongoConnect, isProduction } = require("./services/mongo");
 
 const PORT = process.env.PORT || 3000;
 
 const HTTPServer = http.createServer(app);
 
-const HTTPSServer = https.createServer({
-  cert: fs.readFileSync('cert.pem'),
-  key: fs.readFileSync('key.pem')
-}, app)
+
 
 async function connectToMongoDB() {
   try {
     await mongoConnect();
 
     // Start the Express server after successful MongoDB connection
-    isProduction ?
+    if (isProduction) {
       HTTPServer.listen(PORT, () => {
         console.log(`Listening on port ${PORT}`);
-      }) :
+      })
+    } else {
+
+      const HTTPSServer = https.createServer({
+        cert: fs.readFileSync('cert.pem'),
+        key: fs.readFileSync('key.pem')
+      }, app)
+
       HTTPSServer.listen(PORT, () => {
         console.log(`Listening on port ${PORT}`);
       });
+    }
 
   } catch (err) {
     console.error("MongoDB connection error:", err);
